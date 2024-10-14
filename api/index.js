c1d7fe31aaa511ba7f622d252197f6da034a08d0
Elton <anonymousunknown9856@gmail.com>
2024-10-14 21:44:58 +0530

    backend auth work

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 const app=express();
 dotenv.config();
 
 mongoose.connect(process.env.DB)
 .then(()=>{console.log("connected to db")})
 .catch((err)=>{console.log(err)})
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRouter);
+app.use("/api/user",userRouter);
 
 app.listen(3000,()=>{
     console.log("Server is ruinning on port 3000!")
---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBWk-U62n33YrMA0edFq8w0kwTgijDsEo",
+  authDomain: "estate-70bd7.firebaseapp.com",
+  projectId: "estate-70bd7",
+  storageBucket: "estate-70bd7.appspot.com",
+  messagingSenderId: "160502418203",
+  appId: "1:160502418203:web:77a6bcca66beaa1434a1be"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
